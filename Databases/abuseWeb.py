40b398035671979f517ab5104792a6bf3a340697
paulmahon <paulmahon@192.168.1.169>
2018-04-21 18:01:11 -0400

    parse string from abuse net

--- Databases/abuseWeb.py
+++ Databases/abuseWeb.py
@@ -1,9 +1,15 @@
 import requests
+import re
+from test.pickletester import MyList
 
 
-
-#url = "https://www.abuseipdb.com/check/111.161.35.146/json?key=IoGutYJumuBDzJkWq54qR5Mzpltpg3JjAUQ32JSY&days=30"
-
-response = requests.get("https://www.abuseipdb.com/check/111.161.35.146/json?key=IoGutYJumuBDzJkWq54qR5Mzpltpg3JjAUQ32JSY&days=30")
+response = requests.get("https://www.abuseipdb.com/check/51.144.121.104/json?key=IoGutYJumuBDzJkWq54qR5Mzpltpg3JjAUQ32JSY&days=30")
 
 print(response.content)
+
+myList = str(response.content).split(',')
+
+
+matchW = re.search('"isWhitelisted":false', str(response.content))
+
+print(myList[6])
