4528b343ab3075d5b0db57a552f5a58014006685
paulmahon <paulmahon@192.168.1.169>
2018-04-21 18:31:08 -0400

    test

--- Databases/abuseWeb.py
+++ Databases/abuseWeb.py
@@ -1,5 +1,7 @@
 import requests
 import re
+from test.pickletester import MyList
+
 
 response = requests.get("https://www.abuseipdb.com/check/51.144.121.104/json?key=IoGutYJumuBDzJkWq54qR5Mzpltpg3JjAUQ32JSY&days=30")
 
@@ -7,9 +9,14 @@ print(response.content)
 
 myList = str(response.content).split(',')
 
+
 matchW = re.search('"isWhitelisted":false', str(response.content))
 
+<<<<<<< master
 print(myList[6] + myList[1] + myList[4])
 
 
 print("hello")
+=======
+print(myList[6])
+>>>>>>> 60fffaf last one
