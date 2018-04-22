8615882d2855953adb0a5c97a78e9c0d041dc045
paulmahon <paulmahon@192.168.1.169>
2018-04-22 18:02:28 -0400

    update w/ routine

--- Databases/abuseWeb.py
+++ Databases/abuseWeb.py
@@ -1,17 +1,20 @@
 import requests
 
 
-myipadd = "10.191.195.186"
 
-response = requests.get("https://www.abuseipdb.com/check/"+ myipadd + "/json?key=IoGutYJumuBDzJkWq54qR5Mzpltpg3JjAUQ32JSY&days=30")
 
-if "country" in str(response.content):
 
-    myList = str(response.content).split(',')
-    print(myList[6] + myList[1] + myList[4])
+def get_abuse_status(myipadd ):
+    
+    response = requests.get("https://www.abuseipdb.com/check/"+ myipadd + "/json?key=IoGutYJumuBDzJkWq54qR5Mzpltpg3JjAUQ32JSY&days=30")
+    
+    if "country" in str(response.content):
+    
+        myList = str(response.content).split(',')
+        print(myList[6] + myList[1] + myList[4])
 
 #matchW = re.search('"isWhitelisted":false', str(response.content))
 
 #this is from the imac compouter new new branch
 
-
+get_abuse_status("10.191.195.186")
